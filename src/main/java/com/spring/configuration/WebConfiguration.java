0a079d1f054faff320fd8804e71d9b08cfb384ec
ericshen0521 <ericshen0521@gmail.com>
2020-10-07 17:48:16 -0500

    changed webconfiguration to use non-depricated import

--- src/main/java/com/spring/configuration/WebConfiguration.java
+++ src/main/java/com/spring/configuration/WebConfiguration.java
@@ -6,13 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "com.spring.controller" })
-public class WebConfiguration extends WebMvcConfigurerAdapter{
+public class WebConfiguration implements WebMvcConfigurer{
 
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
